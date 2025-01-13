4abf6437b610e07e2e41d4af06dc752ee3bb374f
Hauken2G <kostasmichopoulos2@gmail.com>
2025-01-13 23:47:36 +0200

    Modals

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // array of books
-const myLibrary = [];
+const Library = [];
 
 // book object constructor
 function Book(title, author, pages, read) {
@@ -25,8 +25,26 @@ function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
+// closes modal on click of x
+const closeModal = document.getElementById("close-modal");
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+  console.log("none");
+});
+// closes modal when clicking away from modal
+const modal = document.getElementById("modal");
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
 
-// loops through the array and displays each book on page
-function bookDisplay() {
-  myLibrary.forEach((book) => {});
-}
+// function displayBook() {
+//  const libraryContainer = document.querySelector(".library-container");
+// Library.forEach((book) => {
+//   const book = Library[i];
+//  const bookCard = document.createElement("div");
+//  bookCard.classList.add("book-card");
+//  });
+// }
+// libraryContainer.appendChild(bookCard);
